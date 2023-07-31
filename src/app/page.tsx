6c8d78cffba2099903ba3e6ccb62a532a49a254b
Olof Andersson <olof.andersson@gmail.com>
2023-07-31 15:44:00 +0200

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,12 @@ export default function Home() {
 				/> :
 				<img src="/barcode-placeholder.svg" alt={'Barcode placeholder'} /> }
 
-			<PatternFormat format="# ###### ######" allowEmptyFormatting mask="_" onChange={handleInput} />
+			<PatternFormat
+				format="# ###### ######"
+				allowEmptyFormatting mask="_"
+				onChange={handleInput}
+				className='font-mono p-2 text-xl'
+			/>
 			{errorMessage && <span>{errorMessage}</span>}
 			<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
 				<p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
