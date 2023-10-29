9d28f3659a5d28b36ffbbdd4a5c1f95f3c0fda43
Olof Andersson <olof.andersson@gmail.com>
2023-10-29 17:52:54 +0100

    template pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const withPWA = require("@ducanh2912/next-pwa").default({
+	dest: "public",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	// https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
 	output: 'export'
 }
 
-module.exports = nextConfig
+module.exports = withPWA({
+	nextConfig
+});
